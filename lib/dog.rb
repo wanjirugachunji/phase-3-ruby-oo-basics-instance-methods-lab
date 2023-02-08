4b43cf624c93e2331df7ee1eeea0f93c3a83b4c4
wanjirugachunji <wanjirugachunji@gmail.com>
2023-02-08 17:15:14 +0300

    done

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+   
+    def bark
+      puts "Woof!"
+    end
+
+  fido = Dog.new
+  fido .bark
+  snoopy=Dog.new
+  fido .bark
+
+  def sit
+    puts "The Dog is sitting"
+  end 
+end 
